7d2f97fa291c4bc7a071ff68421ab63897d1a8b4
nstrucky <nickstruckmeyer@gmail.com>
2019-01-02 20:37:07 -0500

    init

---
+++ database/migrations/2019_01_03_004745_initial.php
@@ -0,0 +1,107 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Initial extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('account_name');
+            $table->string('account_number');
+            $table->text('description');
+            $table->integer('status')->default(0);
+            $table->string('work_orders');
+            $table->date('due_date');
+            $table->timestamps();
+        });
+
+        Schema::create('users', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('job_title');
+            $table->timestamps();
+        });
+
+        Schema::create('project_notes', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('project_id');
+            $table->text('content');
+            $table->timestamps();   
+        });
+
+        Schema::create('user_project', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('project_id');
+            $table->timestamps();
+        });
+
+        Schema::create('tasks', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('status');
+            $table->boolean('complete');
+            $table->string('title');
+            $table->date('due_date');
+            $table->dateTime('completed_on');
+            $table->integer('user_id');
+            $table->integer('project_id');
+            $table->integer('task_template_id');
+            $table->timestamps();
+        });
+
+        Schema::create('status_codes', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('hex_color');
+            $table->timestamps();
+        });
+
+        Schema::create('task_templates', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('roles', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('user_role', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('role_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('projects');
+        Schema::drop('users');
+        Schema::drop('project_notes');
+        Schema::drop('user_project');
+        Schema::drop('roles');
+        Schema::drop('user_role');
+        Schema::drop('tasks');
+        Schema::drop('status_codes');
+        Schema::drop('task_templates');
+    }
+}
