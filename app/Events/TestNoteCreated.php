9e2a93bc57f801e395427c78bafae76a0c9962fa
nstrucky <nickstruckmeyer@gmail.com>
2019-04-14 19:21:04 -0400

    implemnets ShouldBroadcast in TestNoteCreated Event

--- app/Events/TestNoteCreated.php
+++ app/Events/TestNoteCreated.php
@@ -10,7 +10,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class TestNoteCreated 
+class TestNoteCreated implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
